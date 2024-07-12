dd38f2493afb50d094b9858b89a2220be68e33d5
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-07-11 23:47:39 -0400

    create validatePassword.ts

---
+++ src/helpers/validatePassword.ts
@@ -0,0 +1,44 @@
+import bcrypt from 'bcryptjs';
+import { Request } from 'express-validator/lib/base';
+import User from '../models/User';
+
+const validatePassword = (password: string, { req }: Request) => {
+  if (password !== req.body.password_repeat) {
+    throw new Error('User / The passwords do not match - password');
+  }
+
+  return password === req.body.password;
+};
+
+const validatePasswordRepeat = (password: string, { req }: Request) => {
+  if (password !== req.body.password) {
+    throw new Error('User / The passwords do not match - password_repeat');
+  }
+
+  return password === req.body.password_repeat;
+};
+
+const passwordCompare = async (password: string, { req }: Request) => {
+  const { email } = req.body;
+  
+  const user = await User.findOne({ where: { email } });
+
+  if (!user) {
+    throw new Error('The user was not created');
+  }
+
+  // Compare the password provided with the one stored in the database
+  const passwordMatch = await bcrypt.compare(password, user.get().password);
+
+  if (!passwordMatch) {
+    throw new Error('User / Invalid password - Password');
+  }
+
+  return true
+};
+
+export default {
+  validatePassword,
+  validatePasswordRepeat,
+  passwordCompare,
+};
