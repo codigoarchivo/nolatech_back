f2f950a64adf207609da95df58eaf8d3a3a56301
Jackson Quintero <75138506+codigoarchivo@users.noreply.github.com>
2024-07-12 16:16:30 -0400

    refactor limiter

--- src/helpers/rateLimit .ts
+++ src/helpers/rateLimit .ts
@@ -2,6 +2,6 @@ import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 5, // Maximum 5 requests per IP
+  max: 20, // Maximum 20 requests per IP
   message: 'Too many requests from this IP, try again later.',
 });
